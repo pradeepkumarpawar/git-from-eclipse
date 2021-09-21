f931d75193d89def87596ebf86391936c5bfade2
“pradeepkumarpawar” <pradeepkumar.pawar87@gmail.com>
2021-09-21 15:12:49 +0530

    System "S" capital

--- git-from-eclipse/src/main/java/com/pradeep/fsg/HelloApp.java
+++ git-from-eclipse/src/main/java/com/pradeep/fsg/HelloApp.java
@@ -5,7 +5,7 @@ public class HelloApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from git + updated this line...!!");
-		system.out.println("Line added by other developer !!");
+		System.out.println("Line added by other developer !!");
 	}
 
 }
