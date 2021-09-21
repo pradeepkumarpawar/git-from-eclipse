2f22fc202e236a470c9991992deea07f33baa963
“pradeepkumarpawar” <pradeepkumar.pawar87@gmail.com>
2021-09-21 13:55:42 +0530

    First commit from eclipse !

---
+++ git-from-eclipse/src/main/java/com/pradeep/fsg/HelloApp.java
@@ -0,0 +1,10 @@
+package com.pradeep.fsg;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from git");
+	}
+
+}
