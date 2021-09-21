c3ceb14b2c6ea2446490898b6984b2946bb1fb66
“pradeepkumarpawar” <pradeepkumar.pawar87@gmail.com>
2021-09-21 15:07:34 +0530

    second commit after editing !!

--- git-from-eclipse/src/main/java/com/pradeep/fsg/HelloApp.java
+++ git-from-eclipse/src/main/java/com/pradeep/fsg/HelloApp.java
@@ -4,7 +4,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello from git");
+		System.out.println("Hello from git + updated this line...!!");
 	}
 
 }
